88b8bbb152ce8e830e5956e1788a4d340f0a1d47
Wellinton <wellintonpiassa@hotmail.com>
2022-01-11 19:56:16 -0300

    Formatting test report

--- src/TcpSpeedTester.py
+++ src/TcpSpeedTester.py
@@ -1,5 +1,5 @@
 import socket
-import os
+
 import time
 
 class TcpSpeedTester:
@@ -25,12 +25,11 @@ class TcpSpeedTester:
         self.upload_socket.recv(1024) 
         start_time = time.time()
         
-        print("Testing upload...")
+        print("\nTesting upload...")
         while (time.time() - start_time) < self.max_testing_time:
-            bytes_s = self.upload_socket.send(buffer)
+            self.upload_socket.send(buffer)
             self.upload_socket.recv(1024)
             packets_sent += 1
-            #print(f"packet {packets_sent} - {time.time()-start_time} - {bytes_s}...")
         
         self.upload_socket.shutdown(socket.SHUT_RDWR)
         self.upload_socket.close()
@@ -38,12 +37,13 @@ class TcpSpeedTester:
 
         test_time = round(time.time() - start_time, 2)
         print("Upload test finished!")
-        print("Test report:")
-        print(f"bits/s: {round((self.packet_size * packets_sent * 8) / test_time, 2)}")
-        print(f"packets/s: {round(packets_sent / test_time, 2)}")
-        print(f"total bytes transferred: {self.packet_size * packets_sent}")
-        print(f"test time: {test_time}")
-        print(f"lost packets: 0")
+        print("\nTest report:")
+        print("------------")
+        print(f"\tbits/s: {round(((self.packet_size * packets_sent * 8) / test_time) / 1024, 2)}K")
+        print(f"\tpackets/s: {round(packets_sent / test_time, 2)}")
+        print(f"\ttotal bytes transferred: {(self.packet_size * packets_sent)/1024}K")
+        print(f"\ttest time: {test_time} seg")
+        print(f"\tlost packets: 0")
 
 
     def run_download_test(self):
@@ -55,34 +55,32 @@ class TcpSpeedTester:
         connection.send("ok".encode())
         start_time = time.time()
 
-        print("Testing download...")
+        print("\nTesting download...")
         while True:
             bytes_read = connection.recv(self.packet_size)
             if not bytes_read:
                 break
             connection.send("ok".encode())
             packets_received += 1
-            #print(f"packet {packets_received}...")
         
         connection.shutdown(socket.SHUT_RD)
         connection.close()
 
         test_time = round(time.time() - start_time, 2)
         print("Download test finished!")
-        print("Test report:")
-        print(f"bits/s: {round((self.packet_size * packets_received * 8) / test_time, 2)}")
-        print(f"packets/s: {round(packets_received / test_time, 2)}")
-        print(f"total bytes transferred: {self.packet_size * packets_received}")
-        print(f"test time: {test_time}")
-        print(f"lost packets: 0")
+        print("\nTest report:")
+        print("------------")
+        print(f"\tbits/s: {round(((self.packet_size * packets_received * 8) / test_time) / 1024, 2)}K")
+        print(f"\tpackets/s: {round(packets_received / test_time, 2)}")
+        print(f"\ttotal bytes transferred: {(self.packet_size * packets_received)/1024}K")
+        print(f"\ttest time: {test_time} seg")
+        print(f"\tlost packets: 0")
 
 
     def run(self, execution_type):
         if execution_type == "-df":
             self.run_download_test()
-            #sync...
             self.run_upload_test()
         else:
             self.run_upload_test()
-            #sync...
             self.run_download_test()
